8a8850cec752b721f734f1c227c7a7003c95e1c3
DTH1809 <dothaihoa0809@gmail.com>
2024-06-21 21:53:50 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { marked }  from "marked";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: `# Header (H1 size)
+## Sub header (H2 size)
+
+[This is a link](https://www.example.com)
+
+Here is some inline code: \`<div></div>\`
+
+\`\`\`
+// This is a code block
+function sayHello() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+- This is a list item
+
+> This is a blockquote
+
+![Example Image](https://via.placeholder.com/150)
+
+**This is bolded text**
+`
+      
+    };
+    this.handleChange = this.handleChange.bind(this);
+
+    marked.setOptions({
+      gfm: true,
+      breaks: true,
+    });
+  }
+  
+
+  handleChange(event){
+    this.setState({
+      input: event.target.value,
+    })
+  }
+
+  render(){
+    const { input } = this.state;
+    const  markedOutput  = marked.parse(input);
+    return(
+      <div className="App">
+        <div className="editor-container">   
+          
+            <header className="header">EDITOR</header>
+            <textarea className="form-control" rows={10} id="editor" value={input} placeholder="Enter your input" onChange={this.handleChange} />
+          
+        </div>
+        <div className="preview-container">
+          <header className="header">PREVIEWER</header>
+          <div  id="preview" dangerouslySetInnerHTML={{ __html: markedOutput }}></div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
